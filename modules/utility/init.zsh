f503bb1decfec39c8bd71565efd8357be991531a
Robin Mitra <robinmitra1@gmail.com>
2024-11-16 00:15:07 +0000

    Customise grep colours, use 'paradox' theme for Prezto, and add GPG config.

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -129,7 +129,7 @@ fi
 
 # Grep
 if zstyle -t ':prezto:module:utility:grep' color; then
-  export GREP_COLOR=${GREP_COLOR:-'37;45'}            # BSD.
+  export GREP_COLOR=${GREP_COLOR:-'30;44'}            # BSD (this variant makes result black with purple background).
   export GREP_COLORS=${GREP_COLORS:-"mt=$GREP_COLOR"} # GNU.
 
   alias grep="${aliases[grep]:-grep} --color=auto"
